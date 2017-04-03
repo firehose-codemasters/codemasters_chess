67c736d5b1dea5ee0831bc147491d3ed5a58b8f3
chorn <chorn@chorn.com>
2017-04-03 07:39:28 -0400

    Stub some of the logic for Piece
    This is to get you started! You don't have to do it this way if there's
    another you'd prefer, but this is _one_ way to start.

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -8,4 +8,43 @@ class Piece < ApplicationRecord
   validates :x_position, presence: true
   validates :y_position, presence: true
   validates :game_id, presence: true
+
+  def obstructed?(to_x:, to_y:)
+    valid_move?(to_x: to_x, to_y: to_y) &&
+      unobstructed_horizontally?(to_x: to_x, to_y: to_y) &&
+      unobstructed_vertically?(to_x: to_x, to_y: to_y) &&
+      unobstructed_diagonally?(to_x: to_x, to_y: to_y)
+  end
+
+  def valid_move?(to_x:, to_y:)
+    return false if to_x < 0
+    return false if to_y < 0
+  end
+
+  def unobstructed_horizontally?(to_x:, to_y:)
+    !obstructed_horizontally?(to_x: to_x, to_y: to_y)
+  end
+
+  def obstructed_horizontally?(to_x:, to_y:)
+    return false unless valid_move(to_x: to_x, to_y: to_y)
+    raise 'TODO'
+  end
+
+  def unobstructed_vertically?(to_x:, to_y:)
+    !obstructed_vertically?(to_x: to_x, to_y: to_y)
+  end
+
+  def obstructed_vertically?(to_x:, to_y:)
+    return false unless valid_move(to_x: to_x, to_y: to_y)
+    raise 'TODO'
+  end
+
+  def unobstructed_diagonally?(to_x:, to_y:)
+    !obstructed_diagonally?(to_x: to_x, to_y: to_y)
+  end
+
+  def obstructed_diagonally?(to_x:, to_y:)
+    return false unless valid_move(to_x: to_x, to_y: to_y)
+    raise 'TODO'
+  end
 end
--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
-# RSpec.describe Piece, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+describe Piece, type: :model do
+  describe '.new' do
+    it 'allows me to make a new Piece' do
+      expect(Piece.new).to be_a(Piece)
+    end
+  end
+
+  describe '#valid_move?' do
+    let(:piece) { create :piece }
+
+    context 'when I try to move a piece off the board' do
+      it 'has a negative x coordinate' do
+        expect(piece.valid_move?(to_x: -1, to_y: 1)).to eq false
+      end
+
+      it 'has a negative y coordinate' do
+        expect(piece.valid_move?(to_x: 1, to_y: -1)).to eq false
+      end
+    end
+  end
+end
