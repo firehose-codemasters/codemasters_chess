353cdbbff444bdf4453599a87e53e6ff319d6b75
Jack Anderson <phrygian.jack@gmail.com>
2017-04-11 17:22:27 +0000

    Updated diagonal obstruction method and test to work long-distance

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -9,11 +9,46 @@ class Piece < ApplicationRecord
   validates :y_position, presence: true
   validates :game_id, presence: true
 
-  def obstructed_diagonally?(to_x:, to_y:)
-    # Piece will move from (from_x, from_y) to (to_x, to_y)
-    # First, find if there is something in the (to_x, to_y) space
-    # This is for the case of moving one square away:
-    return true if Piece.where(x_position: to_x, y_position: to_y, active: true).exists?
+  def obstructed_diagonally?(from_x:, from_y:, to_x:, to_y:)
+    # Current_x and current_y are used as incrementer variables
+    current_x = from_x
+    current_y = from_y
+    #
+    # Use this IF when destination is up-right
+    if to_x > from_x && to_y > from_y
+      until current_x == to_x && current_y == to_y
+        current_x += 1
+        current_y += 1
+        return true if Piece.where(x_position: current_x, y_position: current_y, active: true).exists?
+      end
+    end
+    #
+    # Use this IF when destination is down-left
+    if to_x < from_x && to_y < from_y
+      until current_x == to_x && current_y == to_y
+        current_x -= 1
+        current_y -= 1
+        return true if Piece.where(x_position: current_x, y_position: current_y, active: true).exists?
+      end
+    end
+    #
+    # Use this IF when destination is up-left
+    if to_x < from_x && to_y > from_y
+      until current_y == to_y && current_x == to_x
+        current_x -= 1 if current_x != to_x
+        current_y += 1 if current_y != to_y
+        return true if Piece.where(x_position: current_x, y_position: current_y, active: true).exists?
+      end
+    end
+    #
+    # Use this IF when destination is down-right
+    if to_x > from_x && to_y < from_y
+      until current_y == to_y && current_x == to_x
+        current_x += 1 if current_x != to_x
+        current_y -= 1 if current_y != to_y
+        return true if Piece.where(x_position: current_x, y_position: current_y, active: true).exists?
+      end
+    end
     false
   end
 end
--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -2,15 +2,33 @@ require 'rails_helper'
 
 RSpec.describe Piece, type: :model do
   describe '#obstructed_diagonally?' do
-    it 'returns true if the move is obstructed' do
+    it 'returns true if the move is obstructed in the up-right direction' do
       moving_piece = FactoryGirl.create(:piece, x_position: 4, y_position: 4)
-      _blocking_piece = FactoryGirl.create(:piece, x_position: 5, y_position: 5)
-      expect(moving_piece.obstructed_diagonally?(to_x: 5, to_y: 5)).to eq(true)
+      _blocking_piece = FactoryGirl.create(:piece, x_position: 6, y_position: 6)
+      expect(moving_piece.obstructed_diagonally?(from_x: 4, from_y: 4, to_x: 8, to_y: 8)).to eq(true)
+    end
+
+    it 'returns true if the move is obstructed in the down-left direction' do
+      moving_piece = FactoryGirl.create(:piece, x_position: 4, y_position: 4)
+      _blocking_piece = FactoryGirl.create(:piece, x_position: 2, y_position: 2)
+      expect(moving_piece.obstructed_diagonally?(from_x: 4, from_y: 4, to_x: 1, to_y: 1)).to eq(true)
+    end
+
+    it 'returns true if the move is obstructed in the up-left direction' do
+      moving_piece = FactoryGirl.create(:piece, x_position: 4, y_position: 4)
+      _blocking_piece = FactoryGirl.create(:piece, x_position: 2, y_position: 6)
+      expect(moving_piece.obstructed_diagonally?(from_x: 4, from_y: 4, to_x: 1, to_y: 7)).to eq(true)
+    end
+
+    it 'returns true if the move is obstructed in the down-right direction' do
+      moving_piece = FactoryGirl.create(:piece, x_position: 4, y_position: 4)
+      _blocking_piece = FactoryGirl.create(:piece, x_position: 6, y_position: 2)
+      expect(moving_piece.obstructed_diagonally?(from_x: 4, from_y: 4, to_x: 7, to_y: 1)).to eq(true)
     end
 
     it 'returns false if the move is not obstructed' do
       moving_piece = FactoryGirl.create(:piece, x_position: 4, y_position: 4)
-      expect(moving_piece.obstructed_diagonally?(to_x: 5, to_y: 5)).to eq(false)
+      expect(moving_piece.obstructed_diagonally?(from_x: 4, from_y: 4, to_x: 8, to_y: 8)).to eq(false)
     end
   end
 end
