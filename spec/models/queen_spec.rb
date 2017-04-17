c8017b627beac42d792d337a516102f73242e017
Jack Anderson <phrygian.jack@gmail.com>
2017-04-17 16:45:50 +0000

    Minor rubocop tweak

--- spec/models/queen_spec.rb
+++ spec/models/queen_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Queen, type: :model do
   describe '#create' do
     it 'creates a queen in the database' do
       queen = FactoryGirl.create(:queen)
-      expect(queen.type == "Queen").to eq(true)
+      expect(queen.type == 'Queen').to eq(true)
     end
   end
 end
