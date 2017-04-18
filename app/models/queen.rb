06d4ad1002fca9502e0fd58a9847582d56a8934e
Jack Anderson <phrygian.jack@gmail.com>
2017-04-18 13:31:57 +0000

    Added move validation tests for queen

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -1,2 +1,5 @@
 class Queen < Piece
+  def valid_queen_move?(to_x:, to_y:)
+    _no_var = "Method goes here #{to_x}, #{to_y}, RuboCop made me do it"
+  end
 end
--- spec/models/queen_spec.rb
+++ spec/models/queen_spec.rb
@@ -7,4 +7,51 @@ RSpec.describe Queen, type: :model do
       expect(queen.type == 'Queen').to eq(true)
     end
   end
+
+  describe '#valid_queen_move?' do
+    it 'returns true if the move is diagonal up-right from the queen' do
+      queen = FactoryGirl.create(:queen, x_position: 4, y_position: 4)
+      expect(queen.valid_queen_move?(to_x: 7, to_y: 7)).to eq(true)
+    end
+
+    it 'returns true if the move is diagonal down-right from the queen' do
+      queen = FactoryGirl.create(:queen, x_position: 4, y_position: 4)
+      expect(queen.valid_queen_move?(to_x: 7, to_y: 1)).to eq(true)
+    end
+
+    it 'returns true if the move is diagonal up-left from the queen' do
+      queen = FactoryGirl.create(:queen, x_position: 4, y_position: 4)
+      expect(queen.valid_queen_move?(to_x: 2, to_y: 6)).to eq(true)
+    end
+
+    it 'returns true if the move is diagonal down-left from the queen' do
+      queen = FactoryGirl.create(:queen, x_position: 4, y_position: 4)
+      expect(queen.valid_queen_move?(to_x: 2, to_y: 2)).to eq(true)
+    end
+
+    it 'returns true if the move is vertical up from the queen' do
+      queen = FactoryGirl.create(:queen, x_position: 4, y_position: 4)
+      expect(queen.valid_queen_move?(to_x: 4, to_y: 7)).to eq(true)
+    end
+
+    it 'returns true if the move is vertical down from the queen' do
+      queen = FactoryGirl.create(:queen, x_position: 4, y_position: 4)
+      expect(queen.valid_queen_move?(to_x: 4, to_y: 1)).to eq(true)
+    end
+
+    it 'returns true if the move is horizontal left from the queen' do
+      queen = FactoryGirl.create(:queen, x_position: 4, y_position: 4)
+      expect(queen.valid_queen_move?(to_x: 1, to_y: 4)).to eq(true)
+    end
+
+    it 'returns true if the move is horizontal right from the queen' do
+      queen = FactoryGirl.create(:queen, x_position: 4, y_position: 4)
+      expect(queen.valid_queen_move?(to_x: 7, to_y: 4)).to eq(true)
+    end
+
+    it 'returns false if the move is not horiz, vert, or diag from the queen' do
+      queen = FactoryGirl.create(:queen, x_position: 4, y_position: 4)
+      expect(queen.valid_queen_move?(to_x: 5, to_y: 7)).to eq(false)
+    end
+  end
 end
