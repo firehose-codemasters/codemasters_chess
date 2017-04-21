5fc2880e43163207fbab4070b8a0307a79041b44
Jim Hinckley <jim@jhwm.net>
2017-04-21 19:57:13 +0000

    Fixed rubocop offenses in king.rb

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,11 +1,9 @@
 # Defines King type Piece and valid movements
 class King < Piece
-
   def valid_move?(to_x:, to_y:)
-    return true if (to_x - x_position).abs == 1 && (to_y - y_position).abs == 0
-    return true if (to_x - x_position).abs == 0 && (to_y - y_position).abs == 1   
-    return true if (to_x - x_position).abs == 1 && (to_y - y_position).abs == 1    
-    else
-      return "You may move the king only one square from its current location! Please try again."
-    end
+    return true if (to_x - x_position).abs == 1 && (to_y - y_position).abs.zero?
+    return true if (to_x - x_position).abs.zero? && (to_y - y_position).abs == 1
+    return true if (to_x - x_position).abs == 1 && (to_y - y_position).abs == 1
+    'You may move the king only one square from its current location! Please try again.'
+  end
 end
