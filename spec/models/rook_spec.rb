4889a5a2d60e94a321c01f9535ff6648b821578d
asmentko <asmentko@gmail.com>
2017-04-22 16:35:15 +0000

    Added additional rspect tests to include moves in all directions

--- spec/models/rook_spec.rb
+++ spec/models/rook_spec.rb
@@ -9,18 +9,33 @@ RSpec.describe Rook, type: :model do
   end
 
   describe '#piece_rules?' do
-    it 'returns true if the move conforms to the allowable horizontal pathway of a rook piece' do
+    it 'returns true if the rook successfully moves to the right along a horizontal pathway' do
       rook = FactoryGirl.create(:rook)
       expect(rook.piece_rules?(to_x: 7, to_y: 3)).to eq(true)
     end  
 
-    it 'returns true if the move conforms to the allowable vertical pathway of a rook piece' do
+    it 'returns true if the rook successfully moves to the left along a horizontal pathway' do
+      rook = FactoryGirl.create(:rook, x_position: 7, y_position: 3)
+      expect(rook.piece_rules?(to_x: 1, to_y: 3)).to eq(true)
+    end 
+
+    it 'returns true if the rook successfully moves up along a vertical pathway' do
       rook = FactoryGirl.create(:rook)
       expect(rook.piece_rules?(to_x: 1, to_y: 5)).to eq(true)
     end
 
-    it 'returns false if the rook piece is moved diagonally' do
+    it 'returns true if the rook successfully moves down along a vertical pathway' do
+      rook = FactoryGirl.create(:rook, x_position: 1, y_position: 5)
+      expect(rook.piece_rules?(to_x: 1, to_y: 1)).to eq(true)
+    end
+
+    it 'returns false if the rook piece is moved diagonally in an upward direction' do
       rook = FactoryGirl.create(:rook)
+      expect(rook.piece_rules?(to_x: 3, to_y: 5)).to eq(false)
+    end
+
+    it 'returns false if the rook piece is moved diagonally in a downward direction' do
+      rook = FactoryGirl.create(:rook, x_position: 6, y_position: 8)
       expect(rook.piece_rules?(to_x: 3, to_y: 5)).to eq(false)
     end
 
