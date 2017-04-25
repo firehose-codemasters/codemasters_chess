6a788dbdf00cb66f06f596c2411bf19194777198
Jim Hinckley <jim@jhwm.net>
2017-04-25 19:41:15 +0000

    Fixed RSpec and Rubocop errors in king_spec.rb, piece.rb and king.rb

--- app/models/king.rb
+++ app/models/king.rb
@@ -4,6 +4,6 @@ class King < Piece
     return true if (to_x - x_position).abs == 1 && (to_y - y_position).abs.zero?
     return true if (to_x - x_position).abs.zero? && (to_y - y_position).abs == 1
     return true if (to_x - x_position).abs == 1 && (to_y - y_position).abs == 1
-    'You may move the king only one square from its current location! Please try again.'
+    'King may move only one square per turn! Please try again.'
   end
 end
--- spec/factories/pieces.rb
+++ spec/factories/pieces.rb
@@ -7,4 +7,31 @@ FactoryGirl.define do
     type ''
     association :game
   end
+
+  factory :king do
+    color 'white'
+    active true
+    x_position 5
+    y_position 1
+    type 'King'
+    association :game
+  end
+
+  factory :queen do
+    color 'white'
+    active true
+    x_position 1
+    y_position 1
+    type 'Queen'
+    association :game
+  end
+
+  factory :rook do
+    color 'white'
+    active true
+    x_position 1
+    y_position 3
+    type 'Rook'
+    association :game
+  end
 end
