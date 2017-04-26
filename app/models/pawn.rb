5f329d25fff2028ba897a5fca4bea10cfedd1bf2
erikyoung <erik.young@rmit.edu.vn>
2017-04-27 00:37:27 +0700

    refactored logic to take into account absolute value so concern over white or black position on board is removed

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,26 +1,15 @@
 class Pawn < Piece
-	attr_accessor :to_x, :to_y
-
-def starting_point_and_move_directions
-	{ white: 2, black: 7} # starting point
-	{ white: 1, black: -1 } # move direction
-end
-
+	
 def valid_move_and_no_way_jose_move?
-	if color == "white" to_y < y_position && to_y > y_position return false#checking for obstructions
-	if color == "white" && to_y > y_position return true
-	if color == "black" to_y < y_position && to_y > y_position return true#checking for obstructions
-	if color == "black" && to_y > y_position return false
-end
-
-def board_space_occupied?(to_x,to_y)
-	unless game.piece_at(to_x,to_y)
-		return true
+	straight_ahead = (to_y - y_position).abs 
+	return true if straight_ahead != 0 && (to_y - y_position).abs == 1
 end
 
 def first_move
- self.first_move to_y > y_position + 1 || self.first_move to_y > y_position + 2 if color == "white"
- self.first_move to_y > y_position - 1 || self.first_move to_y > y_position - 2 if color == "black"
+ first_move = (to_y - y_position).abs == 1 || (to_y - y_position).abs == 2
+end
+
+def enpassant
 end
 
 
