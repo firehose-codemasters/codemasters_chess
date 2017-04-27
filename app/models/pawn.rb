6d2621cbca8acceb62081b6bdcb10e6c83f359c1
erikyoung <erik.young@rmit.edu.vn>
2017-04-28 02:01:47 +0700

    added move logic for pawn

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -3,29 +3,40 @@ class Pawn < Piece
 def valid_move?
 	return true if normal_move?(to_x, to_y)
 	return true if first_move?(first_move, to_y) #how do we keep track of first move
-	return true if capture move?(to_piece, to_y)
-	return true if en_passant?(to_x, to_y)
+	return true if pawn_diagonal?(to_x, to_y)
+	#return true if capture move?(to_piece, to_y)
+	#return true if en_passant?(to_x, to_y)
 	false
 end
 
+private
 
-	private
-
-def normal_move(to_x, to_y)
+def normal_move?(to_x, to_y)
 	return (to_x - x_position == 0) && (to_y - y_position == 1)
 end
 
+def pawn_diagonal?(to_x, to_y)
+ 	return (to_x - x_position == 1) && (to_y - y_position == 1)
+end
 
-def first_move(first_move, to_y)
+
+def first_move?(first_move, to_y)
  first_move = (to_y - y_position).abs == 1 || (to_y - y_position).abs == 2
 end
 
+# Capture logic here
+
+
 def capture_piece(to_piece, to_x, to_y)
-	to_piece = (to_y - y_position).abs == 1 && (to_x - x_position).abs = 
+	
 end
 
-#need to know there is an enemy piece/opposite color piece present piece.where
-#need to 
+
+#need many methods for capture
+	#need to know there is an enemy piece/opposite color piece present piece.where
+	#need to make sure it does not invoke the obstruction logic? 
+	#need to establish the move logic
+#
 
 
 
@@ -50,6 +61,7 @@ end
 
 
 	#how to leverage piece logic? 
+
 
 
 
