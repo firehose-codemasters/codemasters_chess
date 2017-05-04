fe0ecd09b0f1586573f1942aa61cccacb57a521f
Jack Anderson <phrygian.jack@gmail.com>
2017-05-04 17:38:32 +0000

    Refactored pawn logic & added some tests

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,25 +1,42 @@
 class Pawn < Piece
   def valid_pawn_move?(to_x:, to_y:)
-    return true if normal_move?(to_x, to_y)
-    return true if first_move?(to_x, to_y) # How do we keep track of first move
-    return true if pawn_diagonal?(to_x, to_y)
+    return true if direction_of_white_and_black_pawns(to_y) && 
+      (
+        normal_move?(to_x, to_y) ||
+        first_move?(to_x, to_y) ||
+        pawn_diagonal?(to_x, to_y)
+      )
     false
   end
 
   private
 
-  # Returns true if the pawn advances one space from its origin
-  def normal_move?(to_x, to_y)
-    return true if color == 'black' && to_y - y_position == -1 && to_x == x_position
-    return true if color == 'white' && to_y - y_position == 1 && to_x == x_position
+  # This logic makes sure that the pawn can only move forward
+  # White pawns can only move *up* the board
+  # Black pawns can only move *down* the board
+  def direction_of_white_and_black_pawns(to_y)
+    return true if color == 'white' && to_y > y_position
+    return true if color == 'black' && to_y < y_position
     false
   end
 
+  # Returns true if the pawn advances one space from its origin
+  def normal_move?(to_x, to_y)
+    return true if (to_y - y_position).abs == 1 && to_x == x_position
+    false
+  end
+
+  # Returns true if the move is diagonal one space.
   def pawn_diagonal?(to_x, to_y)
     (to_x - x_position).abs == 1 && (to_y - y_position).abs == 1
   end
 
+  # Returns true if it's the pawn's first move
   def first_move?(to_x, to_y)
-    (to_y - y_position).abs == 1 || (to_y - y_position).abs == 2 && (to_x - x_position).abs == 0
+    if color == 'black' && y_position == 7 || color == 'white' && y_position == 2 
+      return true if ((to_y - y_position).abs == 1 || (to_y - y_position).abs == 2) && to_x == x_position
+    else
+      false
+    end
   end
 end
