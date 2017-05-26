56e6a3091084bf8b454f184172ec3bab2686c894
Jack Anderson <phrygian.jack@gmail.com>
2017-05-26 23:19:22 +0000

    Added attr_accessor to kings_pieces variable

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,5 +1,7 @@
 # Defines King type Piece and valid movements
 class King < Piece
+  attr_accessor :kings_pieces
+
   def valid_move?(to_x:, to_y:)
     return true if (to_x - x_position).abs == 1 && (to_y - y_position).abs.zero?
     return true if (to_x - x_position).abs.zero? && (to_y - y_position).abs == 1
@@ -20,8 +22,8 @@ class King < Piece
       i.each do |t|
         # t is the x, i[j] is the y
         @kings_pieces.each do |test_piece|
-          puts test_piece.inspect
-          puts test_piece.move_tests(to_x: t, to_y: i[j])
+          # Run move validation tests on every piece
+          test_piece.move_tests(to_x: t, to_y: i[j])
         end
       end
     end
