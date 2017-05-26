370d40437cde741be98ba820155845f5f6905932
Jack Anderson <phrygian.jack@gmail.com>
2017-05-26 19:50:59 +0000

    Added method to build all possible moves

--- app/models/king.rb
+++ app/models/king.rb
@@ -8,6 +8,22 @@ class King < Piece
   end
 
   def kings_team
-    kings_pieces =Piece.where(color: color, game_id: game_id)
+    @kings_pieces = Piece.where(color: color, game_id: game_id)
+  end
+
+  def possible_moves
+    coords = []
+    8.times do 
+      coords << [*1..8]
+    end
+    coords.each_with_index do |i, j|
+      i.each do |t|
+        # t is the x, i[j] is the y
+        @kings_pieces.each do |test_piece|
+          puts test_piece.inspect
+          puts test_piece.move_tests(to_x: t, to_y: i[j])
+        end
+      end
+    end
   end
 end
