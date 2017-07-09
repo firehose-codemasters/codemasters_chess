89df40755788ae3731f4e7450f029e1f0bfbcb4c
Jack Anderson <phrygian.jack@gmail.com>
2017-07-09 14:12:36 +0000

    Got game ID and coords to appear on squares

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,10 @@
 module GamesHelper
+  def render_piece(game_id, x, y)
+    return "game_id: #{game_id}, x: #{x}, y: #{y}"
+    # return 'piece!' if Piece.find(game_id: game_id, x_position: x, y_position: y, active: true)
+  end
+
+  def current_game
+    @current_game ||= Game.find(params[:id])
+  end
 end
