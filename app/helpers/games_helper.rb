d11e25e26e02fcb5d81bee322bb49594c471a991
Jack Anderson <phrygian.jack@gmail.com>
2017-07-09 14:35:58 +0000

    Got piece to appear on div, and also display piece type

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,10 +1,15 @@
 module GamesHelper
   def render_piece(game_id, x, y)
-    return "game_id: #{game_id}, x: #{x}, y: #{y}"
+    return "PIECE!!!! type: #{piece_at(x, y).type}" if piece_at(x, y)
+    "game_id: #{game_id}, x: #{x}, y: #{y}"
     # return 'piece!' if Piece.find(game_id: game_id, x_position: x, y_position: y, active: true)
   end
 
   def current_game
     @current_game ||= Game.find(params[:id])
+  end
+
+  def piece_at(x, y)
+    current_game.pieces.find_by(x_position: x, y_position: y, active: true)
   end
 end
